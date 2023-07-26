6758fed0dccf181fc7d28451c2ef46ca7e3e724c
Kingsley Oyeoka <kingstoneoy@gmail.com>
2023-07-26 15:39:24 +0100

    choose me transit

--- src/components/Chooseme/Choose.jsx
+++ src/components/Chooseme/Choose.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FaVolleyballBall } from "react-icons/fa";
 import { MdComputer, MdPeopleOutline } from "react-icons/md";
+import { motion } from "framer-motion";
 
 const Choose = () => {
+  const scrollRef = useRef(null);
   return (
     <div className="  w-full py-10 md:py-14 mont ">
       <div className="  w-[90%] md:w-[80%] mx-auto ">
@@ -11,58 +13,82 @@ const Choose = () => {
         </h3>
 
         <div className=" w-full flex flex-col md:flex-row items-center gap-10 md:items-start md:gap-3 flex-wrap">
-          <div className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]">
-            <div className=" w-[80%] mx-auto flex flex-col gap-6">
-              <div className=" flex flex-col items-center pt-5">
-                <span className=" mycolor text-[50px] md:text-[60px]">
-                  <MdPeopleOutline />
-                </span>
-                <h3 className=" capitalize font-bold myshade pt-4 text-lg">
-                  Team Player
-                </h3>
-              </div>
+          <div ref={scrollRef}>
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: false }}
+              className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]"
+            >
+              <div className=" w-[80%] mx-auto flex flex-col gap-6">
+                <div className=" flex flex-col items-center pt-5">
+                  <span className=" mycolor text-[50px] md:text-[60px]">
+                    <MdPeopleOutline />
+                  </span>
+                  <h3 className=" capitalize font-bold myshade pt-4 text-lg">
+                    Team Player
+                  </h3>
+                </div>
 
-              <div className="text-[12px] md:text-[14px] myshade">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                <div className="text-[12px] md:text-[14px] myshade">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                </div>
               </div>
-            </div>
+            </motion.div>
           </div>
 
-          <div className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]">
-            <div className=" w-[80%] mx-auto flex flex-col gap-6">
-              <div className=" flex flex-col items-center pt-5">
-                <span className=" mycolor text-[50px] md:text-[60px]">
-                  <FaVolleyballBall />
-                </span>
-                <h3 className=" capitalize font-bold myshade pt-4 text-lg">
-                  Creative Design
-                </h3>
-              </div>
+          <div ref={scrollRef}>
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: false }}
+              className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]"
+            >
+              <div className=" w-[80%] mx-auto flex flex-col gap-6">
+                <div className=" flex flex-col items-center pt-5">
+                  <span className=" mycolor text-[50px] md:text-[60px]">
+                    <FaVolleyballBall />
+                  </span>
+                  <h3 className=" capitalize font-bold myshade pt-4 text-lg">
+                    Creative Design
+                  </h3>
+                </div>
 
-              <div className="text-[12px] md:text-[14px] myshade">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                <div className="text-[12px] md:text-[14px] myshade">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                </div>
               </div>
-            </div>
+            </motion.div>
           </div>
 
-          <div className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]">
-            <div className=" w-[80%] mx-auto flex flex-col gap-6">
-              <div className=" flex flex-col items-center pt-5">
-                <span className=" mycolor text-[50px] md:text-[60px]">
-                  <MdComputer />
-                </span>
-                <h3 className=" capitalize font-bold myshade pt-4 text-lg">
-                  Responsive
-                </h3>
-              </div>
+          <div ref={scrollRef}>
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: false }}
+              className=" mx-auto w-[90%] h-[250px] md:w-[350px] md:h-70  rounded-md shadow-2xl bg-[#CCC]"
+            >
+              <div className=" w-[80%] mx-auto flex flex-col gap-6">
+                <div className=" flex flex-col items-center pt-5">
+                  <span className=" mycolor text-[50px] md:text-[60px]">
+                    <MdComputer />
+                  </span>
+                  <h3 className=" capitalize font-bold myshade pt-4 text-lg">
+                    Responsive
+                  </h3>
+                </div>
 
-              <div className="text-[12px] md:text-[14px] myshade">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                <div className="text-[12px] md:text-[14px] myshade">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repellat quaerat expedita placeat fugit impedit at, voluptatum
+                </div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
