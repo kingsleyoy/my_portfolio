16bc06d4213d3ca17cb8ecf29bad2cc66863cdcc
Kingsley Oyeoka <kingstoneoy@gmail.com>
2023-07-30 17:08:41 +0100

    kkk

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import Aboutme from "../../assets/kingsabout.png";
+import Aboutme from "../../assets/abt.png";
 import { motion } from "framer-motion";
 
 const About = () => {
